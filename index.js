33dc3d115549ac3bf622283ab071087d295a7cb9
Muhammed Sihebi <muhammednurpro@gmail.com>
2022-04-12 02:38:30 +0300

    finish assignement

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = "bob"; 
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'; 
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "int"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "asdf"
+}
+
+
+
+if (false){
+    var x = 10; 
+}else{
+    console.log(x);
+}
